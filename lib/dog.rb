2bdb79f532599fb42a7a32e60264dcb057a6e08c
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-18 18:19:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,18 @@
-class Dog 
-  
-  def initialize(dog_name)
-    @dog_name = dog_name
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
-  
-  def name=(dog_name)
-    this_dog_name
-    
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
   end 
-  
-end
+end 
